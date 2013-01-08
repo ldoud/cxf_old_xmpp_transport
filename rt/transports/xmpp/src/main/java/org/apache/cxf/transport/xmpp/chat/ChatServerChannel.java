99b639b16662624ffd5da0da2e49e8a40f5c3db7
Leon Doud <leon.doud@gmail.com>
2013-01-07 23:25:47 -0500

    Refactoring and starting with chat.

---
+++ rt/transports/xmpp/src/main/java/org/apache/cxf/transport/xmpp/chat/ChatServerChannel.java
@@ -0,0 +1,91 @@
+/**
+ * Licensed to the Apache Software Foundation (ASF) under one
+ * or more contributor license agreements. See the NOTICE file
+ * distributed with this work for additional information
+ * regarding copyright ownership. The ASF licenses this file
+ * to you under the Apache License, Version 2.0 (the
+ * "License"); you may not use this file except in compliance
+ * with the License. You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing,
+ * software distributed under the License is distributed on an
+ * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+ * KIND, either express or implied. See the License for the
+ * specific language governing permissions and limitations
+ * under the License.
+ */
+
+package org.apache.cxf.transport.xmpp.chat;
+
+import java.io.ByteArrayInputStream;
+import java.io.InputStream;
+import java.util.logging.Logger;
+
+import org.apache.cxf.common.logging.LogUtils;
+import org.apache.cxf.message.Exchange;
+import org.apache.cxf.message.ExchangeImpl;
+import org.apache.cxf.message.MessageImpl;
+import org.apache.cxf.transport.MessageObserver;
+import org.apache.cxf.transport.xmpp.strategy.MessageReceiptStrategy;
+import org.jivesoftware.smack.Chat;
+import org.jivesoftware.smack.ChatManagerListener;
+import org.jivesoftware.smack.MessageListener;
+
+public class ChatServerChannel implements MessageReceiptStrategy, MessageListener, ChatManagerListener {
+    
+    private static final Logger LOGGER = LogUtils.getLogger(ChatServerChannel.class);
+    
+    // This object triggers the Apache CXF processing of a SOAP message.
+    private MessageObserver cxfMsgObserver;
+    
+    /**
+     * Called when another XMPP client starts a session with the service.
+     * Start listening to the chat session in order to receive SOAP messages.
+     * Required the the XMPP API (Smack) interface ChatManagerListener
+     * {@inheritDoc}
+     */
+    @Override
+    public void chatCreated(Chat chatSession, boolean createdLocally) {
+        chatSession.addMessageListener(this);
+    }
+
+    /**
+     * Called when a XMPP chat message is received.
+     * Required the the XMPP API (Smack) interface MessageListener
+     * {@inheritDoc}
+     */
+    @Override
+    public void processMessage(Chat chatSession, org.jivesoftware.smack.packet.Message xmppChatMsg) {
+        LOGGER.info("Processing chat message: " + xmppChatMsg.getBody());
+        
+        // The contents of the XMPP message is a SOAP message.
+        // Put the SOAP message into a CXF message.
+        org.apache.cxf.message.Message cxfMsg = new MessageImpl();
+        cxfMsg.setContent(InputStream.class, 
+                          new ByteArrayInputStream(xmppChatMsg.getBody().getBytes()));
+
+        // The back channel is how the return value is sent to the client.
+        // So its necessary to use the same chat session that received the message.
+        Exchange msgExchange = new ExchangeImpl();
+        msgExchange.setConduit(new ChatServerReplyChannel(chatSession));
+        cxfMsg.setExchange(msgExchange);
+
+        getMessageObserver().onMessage(cxfMsg);
+    }
+    
+    @Override
+    public synchronized void setMessageObserver(MessageObserver observer) {
+        cxfMsgObserver = observer;
+    }
+    
+    /**
+     * The observer maybe changed on the fly.
+     * Use this method to make sure the current reference is used.
+     * @return The observer that pushes the SOAP message through the CXF stack.
+     */
+    protected synchronized MessageObserver getMessageObserver() {
+        return cxfMsgObserver;
+    }
+}
---
+++ rt/transports/xmpp/src/test/java/test/xmpp/service/HelloWorldService.java
@@ -0,0 +1,40 @@
+/**
+ * Licensed to the Apache Software Foundation (ASF) under one
+ * or more contributor license agreements. See the NOTICE file
+ * distributed with this work for additional information
+ * regarding copyright ownership. The ASF licenses this file
+ * to you under the Apache License, Version 2.0 (the
+ * "License"); you may not use this file except in compliance
+ * with the License. You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing,
+ * software distributed under the License is distributed on an
+ * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+ * KIND, either express or implied. See the License for the
+ * specific language governing permissions and limitations
+ * under the License.
+ */
+
+package test.xmpp.service;
+
+import javax.jws.WebParam;
+import javax.jws.WebService;
+import javax.xml.ws.BindingType;
+
+@WebService(endpointInterface = "test.xmpp.service.HelloWorld", serviceName = "HelloWorld")
+@BindingType(value = "http://www.w3.org/2003/05/soap/bindings/HTTP/")
+public class HelloWorldService implements HelloWorld {
+    
+    @Override
+    public String sayHi(@WebParam(name = "text") String text) {
+        System.out.println("Hello " + text);
+        return "Hello " + text;
+    }
+
+    @Override
+    public void yell(String loudMsg) {
+        System.out.println("Yelling: " + loudMsg);
+    }
+}
